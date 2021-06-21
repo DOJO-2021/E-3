62120e89d808dee2ec31fe0d6ecd518e73a9c996
dojo2021-y-watanuki <watanuki@seplus2016.onmicrosoft.com>
2021-06-21 09:12:59 +0900

    6月21日 更新

---
+++ SMSystem/WebContent/WEB-INF/jsp/changepassword2.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+      <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>パスワード変更 | SMSystem</title>
+</head>
+<body>
+<c:forEach var="e" items= "${userInfo}">
+<p>現パスワード</p>
+<h4>${e.user_pw}</h4>
+
+<form method = "POST" action = "/SMSystem/ChangePasswordServlet2">
+<input type = "hidden" name = "user_id" value = "${e.user_id}">
+<p>新パスワード</p>
+<input type = "text" name = "new_pw" >
+<p>確認</p>
+<input type = "text" name = "comfirm_pw">
+<input type = "submit" name = "change" value = "変更">
+</form>
+</c:forEach>
+
+</body>
+</html>
---
+++ SMSystem/src/servlet/ChangePasswordServlet2.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import model.User;
+
+/**
+ * Servlet implementation class ChangePasswordServlet
+ */
+@WebServlet("/ChangePasswordServlet2")
+public class ChangePasswordServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.getAttribute("userInfo");
+		session.getAttribute("id");
+		RequestDispatcher dispatcher =request.getRequestDispatcher("/WEB-INF/jsp/changepassword2.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String new_password = request.getParameter("new_pw");
+		String user_id = request.getParameter("user_id");
+
+		UserDao UDao = new UserDao();
+		if(UDao.update_user(new User(0,user_id,new_password,"","","","","",""))) {
+			HttpSession session = request.getSession();
+			session.getAttribute("userInfo");
+			session.removeAttribute("userInfo");
+			List<User> user_info = UDao.select_user(new User(0,user_id,"","","","","","",""));
+			session.setAttribute("userInfo",user_info);
+			response.sendRedirect("/SMSystem/PasswordOkServlet2");
+		}else {
+			response.sendRedirect("/SMSystem/PasswordNoServlet2");
+		}
+	}
+
+}
